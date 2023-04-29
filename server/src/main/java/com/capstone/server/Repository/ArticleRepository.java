edd8c077cee000ba44af7b15eb64b6002bcaa1e4
YejiGong <yejigong00@gmail.com>
2023-04-29 15:57:44 +0900

    Test: 테스트 코드 작성
    테스트 코드 작성 및 쿼리 수정
    
    fix #13, close #10

--- server/src/main/java/com/capstone/server/Repository/ArticleRepository.java
+++ server/src/main/java/com/capstone/server/Repository/ArticleRepository.java
@@ -11,6 +11,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ArticleRepository extends JpaRepository<Article, String> {
 
-    @Query("select a.atcTitle, a.atcWriter, a.atcPhotoIn, a.difficulty, a.genre from Article a left outer join TbRead t on a.id=t.tbArticleId where t.tbUserId = :id and a.genre = :genre and t.tbArticleId is NULL")
+    @Query("select a.atcTitle as atcTitle, a.atcWriter as atcWriter, a.atcPhotoIn as atcPhotoIn, a.difficulty as difficulty, a.genre as genre from Article a left outer join TbRead t on t.tbUserId=:id and a.id = t.tbArticleId where t.tbArticleId is NULL and a.genre = :genre")
     Page<ArticleInterface> findByArticleLEFTJOINTbRead(String id, String genre, Pageable pageable);
 }
--- server/src/test/java/com/capstone/server/Service/ArticleServiceTest.java
+++ server/src/test/java/com/capstone/server/Service/ArticleServiceTest.java
@@ -1,7 +1,72 @@
 package com.capstone.server.Service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.capstone.server.Interface.ArticleInterface;
+import com.capstone.server.Repository.ArticleRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.context.ContextConfiguration;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+@ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class)
 class ArticleServiceTest {
+    @Mock
+    ArticleRepository articleRepository;
+    @InjectMocks
+    ArticleService articleService;
+
+    @Test
+    void articleList_test(){
+        Pageable pageable = PageRequest.of(0, 1);
+        List<ArticleInterface> result = new ArrayList<>();
+
+        ArticleInterface article = new ArticleInterface() {
+            @Override
+            public String getAtcTitle() {
+                return "title1";
+            }
+
+            @Override
+            public String getAtcWriter() {
+                return "writer";
+            }
+
+            @Override
+            public String getAtcPhotoIn() {
+                return "http://url.com";
+            }
+
+            @Override
+            public String getDifficulty() {
+                return "D1";
+            }
+
+            @Override
+            public String getGenre() {
+                return "art";
+            }
+        };
+        result.add(article);
+        Page<ArticleInterface> page = new PageImpl<>(result);
+        when(articleRepository.findByArticleLEFTJOINTbRead(anyString(), anyString(), any()))
+                .thenReturn(page);
+        List<ArticleInterface> result_list = articleService.articleList("test123", "art", 0, 1);
+        assertThat(result_list.get(0).getAtcTitle()).isEqualTo("title1");
+    }
 
 }
