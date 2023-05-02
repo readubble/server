f0b580027d4000571d2b9e17cfe28b62c3694505
YejiGong <yejigong00@gmail.com>
2023-05-02 17:46:51 +0900

    Feat: 어휘 퀴즈 결과 기능
    사용자의 어휘 퀴즈 결과 기능을 반환함
    
    fix #29

--- server/src/main/java/com/capstone/server/Service/WordQuizAnswerService.java
+++ server/src/main/java/com/capstone/server/Service/WordQuizAnswerService.java
@@ -1,23 +1,44 @@
 package com.capstone.server.Service;
 
 import com.capstone.server.DTO.RequestDTO.WordQuizRequestDTO;
+import com.capstone.server.Domain.WordQuiz;
 import com.capstone.server.Domain.WordQuizAnswer;
 import com.capstone.server.Repository.WordQuizAnswerRepository;
+import com.capstone.server.Repository.WordQuizRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
 public class WordQuizAnswerService {
     private final WordQuizAnswerRepository wordQuizAnswerRepository;
+    private final WordQuizRepository wordQuizRepository;
     @Autowired
-    public WordQuizAnswerService(WordQuizAnswerRepository wordQuizAnswerRepository){
+    public WordQuizAnswerService(WordQuizAnswerRepository wordQuizAnswerRepository, WordQuizRepository wordQuizRepository){
         this.wordQuizAnswerRepository = wordQuizAnswerRepository;
+        this.wordQuizRepository = wordQuizRepository;
     }
 
     public void wordQuizSave(WordQuizRequestDTO wordQuizRequestDTO){
         WordQuizAnswer wordQuizAnswer = new WordQuizAnswer(wordQuizRequestDTO.getUserId(), wordQuizRequestDTO.getQuizId(), wordQuizRequestDTO.getQuizChoice(), wordQuizRequestDTO.getQuizResult());
         wordQuizAnswerRepository.save(wordQuizAnswer);
+    }
+
+    public String wordQuizInfo(String userId){
+        List<WordQuiz> quiz = wordQuizRepository.findTop3By();
+        String result="";
+        for(int i=0; i<3; i++){
+            Optional<WordQuizAnswer> wordQuizAnswer = wordQuizAnswerRepository.findByTbUserIdAndTbWordQuizQuizNo(userId, quiz.get(i).getQuizNo());
+            if(wordQuizAnswer.isPresent() && wordQuizAnswer.get().getCorrectFl().equals("Y")){
+                result+="T";
+            }else{
+                result+="F";
+            }
+        }
+        return result;
     }
 }
