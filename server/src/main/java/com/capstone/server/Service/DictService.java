52842c92e0728087016c3d426dc9022ea97ee9b3
YejiGong <yejigong00@gmail.com>
2023-05-03 15:23:25 +0900

    Fix: 사전 검색(open api) 기능
    open api를 이용해서 사전 검색 결과를 반환해주는 기능
    
    fix #28

--- server/src/main/java/com/capstone/server/Service/DictService.java
+++ server/src/main/java/com/capstone/server/Service/DictService.java
@@ -25,6 +25,7 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -39,76 +40,88 @@ public class DictService {
         this.searchRepository = searchRepository;
     }
 
-//    @Builder
-//    @Data
-//    private static class Response{
-//        Boolean status;
-//        ResponseEntity<String> response;
-//
-//        public Response(Boolean status){
-//            this.status = status;
-//        }
-//        public Response(Boolean status, ResponseEntity<String> response){
-//            this.status = status;
-//            this.response = response;
-//        }
-//    }
-//
-//    private Response getDictApi(String keyword, int start, int num) throws IOException{
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
-//
-//        HttpEntity<MultiValueMap<String, String>> DictRequest = new HttpEntity<>(headers);
-//        RestTemplate restTemplate = new RestTemplate();
-//        try{
-//            ResponseEntity<String> response = restTemplate.exchange(
-//                    "https://stdict.korean.go.kr/api/search.do?key="+DictKey.DictKey+"?q="+keyword+"?req_type=json"+"?start="+start+"?num="+num,
-//                    HttpMethod.GET,
-//                    DictRequest,
-//                    String.class
-//            );
-//            return new Response(true, response);
-//        }catch (HttpClientErrorException e){
-//            return new Response(false);
-//            //db에서 받아오기
-//        }
-//    }
-//
-//    public Boolean checkStatus(Response response){
-//        if(response.status && response.response.getStatusCode().equals(HttpStatus.OK)){
-//            return true;
-//        }else{
-//            return false;
-//        }
-//    }
-//
-//    public List<DictDTO> getDictInfo(String keyword, String userId) throws IOException {
-//        Response response = getDictApi(keyword, 1, 10);
-//        if(checkStatus(response)){ //응답 정상
-//            String responseBody = response.response.getBody();
-//            ObjectMapper objectMapper = new ObjectMapper();
-//            JsonNode jsonNode = objectMapper.readTree(responseBody);
-//
-//            if(jsonNode.get("channel").get("total").asInt()>jsonNode.get("channel").get("total").asInt()){
-//                //total이 큰 경우 -> 목록 다시 받아오기
-//                response = getDictApi(keyword, 1, jsonNode.get("channel").get("total").asInt());
-//                if(!checkStatus(response)){
-//                    //DB에서 받아오기
-//                }
-//            }
-//
-//            //total이 커서 재시도 후 정상적으로 값을 받아왔거나, total이 num보다 적은 경우
-//            //API의 결과값을 그대로 반환해주면 된다.
-//
-//        }else{ //서버 통신 오류 -> DB에서 받아오기
-//
-//        }
-//    }
+    @Builder
+    @Data
+    private static class Response{
+        Boolean status;
+        ResponseEntity<String> response;
+
+        public Response(Boolean status){
+            this.status = status;
+        }
+        public Response(Boolean status, ResponseEntity<String> response){
+            this.status = status;
+            this.response = response;
+        }
+    }
+
+    private Response getDictApi(String keyword, int start, int num) throws IOException{
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Content-type","application/x-www-form-urlencoded;charset=utf-8");
+
+        HttpEntity<MultiValueMap<String, String>> DictRequest = new HttpEntity<>(headers);
+        RestTemplate restTemplate = new RestTemplate();
+        try{
+            ResponseEntity<String> response = restTemplate.exchange(
+                    "https://stdict.korean.go.kr/api/search.do?key="+DictKey.DictKey+"?q="+keyword+"?req_type=json"+"?start="+start+"?num="+num,
+                    HttpMethod.GET,
+                    DictRequest,
+                    String.class
+            );
+            return new Response(true, response);
+        }catch (HttpClientErrorException e){
+            return new Response(false);
+            //db에서 받아오기
+        }
+    }
+
+    public Boolean checkStatus(Response response){
+        if(response.status && response.response.getStatusCode().equals(HttpStatus.OK)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public List<DictDTO> getDictInfo(String keyword, String userId) throws IOException {
+        List<DictDTO> result = new ArrayList<>();
+        Response response = getDictApi(keyword, 1, 10);
+        if(checkStatus(response)){ //응답 정상
+            String responseBody = response.response.getBody();
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(responseBody);
+
+            if(jsonNode.get("channel").get("total").asInt()>jsonNode.get("channel").get("total").asInt()){
+                //total이 큰 경우 -> 목록 다시 받아오기
+                response = getDictApi(keyword, 1, jsonNode.get("channel").get("total").asInt());
+                if(!checkStatus(response)){
+                    //DB에서 받아오기
+                    return getDictInfoDB(keyword, userId);
+                }
+            }
+
+            //total이 커서 재시도 후 정상적으로 값을 받아왔거나, total이 num보다 적은 경우
+            //API의 결과값을 그대로 반환해주면 된다.
+            Date date = new Date();
+            List<JsonNode> items = jsonNode.get("channel").findValues("item");
+            for(int i=0; i<items.size(); i++){
+                DictDTO dictDTO = new DictDTO(items.get(i).get("target_code").asInt(), items.get(i).get("word").asText(),
+                        items.get(i).get("container").get("definition").asText(), "");
+                result.add(dictDTO);
+                Search search = new Search(userId, result.get(i).getWordNo(), "N", date);
+                searchRepository.save(search);
+            }
+            return result;
+
+        }else{ //서버 통신 오류 -> DB에서 받아오기
+            return getDictInfoDB(keyword, userId);
+
+        }
+    }
 
     public List<DictDTO> getDictInfoDB(String keyword, String userId){
         List<DictDTO> result = dictRepository.findAllByWordNm(keyword);
         Date date = new Date();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yy-MM-dd");
         for(int i=0; i<result.size(); i++){
             Search search = new Search(userId, result.get(i).getWordNo(), "N", date);
             searchRepository.save(search);
--- server/src/test/java/com/capstone/server/Controller/WordControllerTest.java
+++ server/src/test/java/com/capstone/server/Controller/WordControllerTest.java
@@ -1,4 +1,80 @@
+package com.capstone.server.Controller;
+
+import com.capstone.server.DTO.DictDTO;
+import com.capstone.server.Domain.User;
+import com.capstone.server.Repository.UserRepository;
+import com.capstone.server.Service.ArticleService;
+import com.capstone.server.Service.DictService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.simple.JSONObject;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.test.context.support.WithUserDetails;
+import org.springframework.test.web.servlet.MockMvc;
+
+import javax.annotation.PostConstruct;
+import java.util.Date;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest(webEnvironment =  SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 class WordControllerTest {
-  
+    @Autowired
+    MockMvc mvc;
+    @Autowired
+    ObjectMapper objectMapper;
+    @MockBean
+    DictService dictService;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    BCryptPasswordEncoder bCryptPasswordEncoder;
+    static DictDTO dict1;
+    static DictDTO dict2;
+
+    @PostConstruct
+    void setting(){
+        userRepository.save(User.builder()
+                .id("test123")
+                .userNm("tester")
+                .userPw(bCryptPasswordEncoder.encode("1234"))
+                .joinDt(new Date())
+                .userLevel("L1")
+                .userExp(0)
+                .userPhotoIn("")
+                .role("ROLE_USER").build());
+        dict1 = new DictDTO(1, "사과", "살이 연하고 달며 물이 많은 참외", "사과를 먹다");
+        dict2 = new DictDTO(2, "사과", "조선 시대에, 오위에 둔 정육품의 군직. 현직에 종사하고 있찌 않은 문관, 무관 및 음관이 맡았다.", "");
+
+    }
+
+    @Test
+    @WithUserDetails("test123")
+    void wordSearchTest() throws Exception {
+        when(dictService.getDictInfoDB("사과", "test123")).thenReturn(List.of(dict1, dict2));
+        JSONObject jsonObject = new JSONObject();
+        jsonObject.put("id", "test123");
+        jsonObject.put("keyword", "사과");
+        String content = objectMapper.writeValueAsString(jsonObject);
+
+        mvc.perform(post("/word")
+                        .header("Authorization", "token")
+                        .content(content)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.data[0].wordNo").value(1));
+    }
+
 }
